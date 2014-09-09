d2d0a5f27b1d3527cd6be659167c0189de2fadab
Aidan Cornelius-Bell <aidan@acproduction.com.au>
2014-09-09 22:29:46 +0930

    Events List API

---
+++ api/listClasses.php
@@ -0,0 +1,26 @@
+<?php
+
+require "authentication_header.fnc.php";
+
+require "api.fnc.php";
+
+require "settings.php";
+
+$db = configure_active_database();
+
+$socket = ConnectToDatabase($db);
+
+$query = MakeDatabaseQuery("SELECT * FROM `events`;", $socket);
+
+$return = array();
+
+foreach ($query as $key) {
+	$when = "From " . $key[dtbegin] . " to " . $key[dtend] . ".";
+	$url = "https://events.tfel.edu.au/attendees/?view_event=" . $key[id];
+	$topush = array("id" => $key['id'], "when" => $when, "name" => $key['name'], "cost" => $key['cost'], "catering" => $key['catering'], "detailsURL" => $url, );
+	array_push($return, $topush);
+}
+
+echo json_encode($return, JSON_PRETTY_PRINT);
+
+?>
