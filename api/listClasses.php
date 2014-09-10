98e20baf812efb9f8e3c8fe2fc9b182d6ffdc908
Aidan Cornelius-Bell <aidan@acproduction.com.au>
2014-09-10 11:18:48 +0930

    Better text

--- api/listClasses.php
+++ api/listClasses.php
@@ -16,7 +16,7 @@ $return = array();
 
 foreach ($query as $key) {
 	$when_from = "From " . $key[dtbegin];
-	$when_to = "To: " . $key[dtend];
+	$when_to = "Until " . $key[dtend];
 	$start = "Event on " . substr($key[dtbegin], 0, -9);
 	$url = "https://events.tfel.edu.au/attendees/?view_event=" . $key[id] . "&api=1";
 	$topush = array("id" => $key['id'], "when_from" => $when_from, "when_to" => $when_to, "start" => $start, "where" => $key['venue'], "name" => $key['name'], "cost" => $key['cost'], "catering" => $key['catering'], "detailsURL" => $url, );
