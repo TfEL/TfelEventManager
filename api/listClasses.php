d415e0073a9095d8f9983946aa64a9a51a85da1e
Aidan Cornelius-Bell <aidan@acproduction.com.au>
2014-09-10 11:13:42 +0930

    Fix substr

--- api/listClasses.php
+++ api/listClasses.php
@@ -17,7 +17,7 @@ $return = array();
 foreach ($query as $key) {
 	$when_from = "From " . $key[dtbegin];
 	$when_to = "To: " . $key[dtend];
-	$start = substr($key[dtbegin], -9, 9);
+	$start = substr($key[dtbegin], 0, -9);
 	$url = "https://events.tfel.edu.au/attendees/?view_event=" . $key[id] . "&api=1";
 	$topush = array("id" => $key['id'], "when_from" => $when_from, "when_to" => $when_to, "start" => $start, "where" => $key['venue'], "name" => $key['name'], "cost" => $key['cost'], "catering" => $key['catering'], "detailsURL" => $url, );
 	array_push($return, $topush);
